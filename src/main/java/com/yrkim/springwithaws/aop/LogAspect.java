35205f56da7265f48c637c98dbad1e696b8e36c3
yeongroke <chfaos120@naver.com>
2022-04-05 01:03:20 +0900

    conf: log aspect add

---
+++ src/main/java/com/yrkim/springwithaws/aop/LogAspect.java
@@ -0,0 +1,33 @@
+package com.yrkim.springwithaws.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAspect implements Ordered {
+    @Around("execution( * com.yrkim.springwithaws.controller.*.*(..))")
+    public Object doAroundRequest(ProceedingJoinPoint point) throws Throwable {
+        long start = System.currentTimeMillis();
+        log.debug("This : '" + point.getThis() + "'");
+        log.debug("Kind : '" + point.getKind() + "'");
+        log.debug("Args : '" + point.getArgs() + "'");
+        log.debug("Target: '" + point.getTarget() + "'");
+
+        long finish = System.currentTimeMillis();
+        long timeMs = finish - start;
+        log.debug("[ " + point.getSignature().getDeclaringTypeName() + " ] :" + point.getSignature().getName() + "()'");
+        log.debug("Doing Time - " + timeMs/1000 + " Second");
+        return point.proceed();
+    }
+
+    @Override
+    public int getOrder() {
+        return -1000;
+    }
+}
