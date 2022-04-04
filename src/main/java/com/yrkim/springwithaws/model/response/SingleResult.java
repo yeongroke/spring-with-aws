0d31b818acb197e00b25e648436a8ba7cb9c84e9
yeongroke <chfaos120@naver.com>
2022-04-05 01:03:49 +0900

    conf: response dto add

---
+++ src/main/java/com/yrkim/springwithaws/model/response/SingleResult.java
@@ -0,0 +1,11 @@
+package com.yrkim.springwithaws.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SingleResult<T> extends CommonResult {
+    private T result;
+}
+
