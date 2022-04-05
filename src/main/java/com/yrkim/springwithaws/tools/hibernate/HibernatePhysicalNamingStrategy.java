d1684620588d8aa18acf79b686b344e35d6609cd
yeongroke <chfaos120@naver.com>
2022-04-05 23:40:26 +0900

    conf: hibernate 로그 커스텀

---
+++ src/main/java/com/yrkim/springwithaws/tools/hibernate/HibernatePhysicalNamingStrategy.java
@@ -0,0 +1,40 @@
+package com.yrkim.springwithaws.tools.hibernate;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+public class HibernatePhysicalNamingStrategy implements PhysicalNamingStrategy {
+
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return name == null ? null : this.convertToUpperCase(name);
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return name == null ? null : this.convertToUpperCase(name);
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return this.convertToUpperCase(name);
+    }
+
+    @Override
+    public Identifier toPhysicalSequenceName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return this.convertToUpperCase(name);
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return this.convertToUpperCase(name);
+    }
+
+    private Identifier convertToUpperCase(final Identifier name) {
+        String regex ="([a-z])([A-Z])";
+        String replacement = "$1_$2";
+        String newName = name.getText().replaceAll(regex, replacement).toUpperCase();
+        return Identifier.toIdentifier(newName);
+    }
+}
