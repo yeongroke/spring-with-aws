ec98a783a4f27225bf7e9d9b3a5d25a678563ee9
yeongroke <chfaos120@naver.com>
2022-04-05 23:39:53 +0900

    conf: transaction config 추가 ( 우선 주석 )

---
+++ src/main/java/com/yrkim/springwithaws/config/TransactionConfig.java
@@ -0,0 +1,12 @@
+package com.yrkim.springwithaws.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/*@Configuration
+@ImportResource({"classpath:/aop.xml"})*/
+public class TransactionConfig {
+    public TransactionConfig() {
+
+    }
+}
