b535bde2cf3270e205ff6e2e47f9d62f206560ef
yeongroke <chfaos120@naver.com>
2022-04-25 02:23:49 +0900

    etc: querydsl 구조 셋팅

---
+++ src/main/java/com/yrkim/springwithaws/order/repository/querydsl/OrderQuerydslRepository.java
@@ -0,0 +1,7 @@
+package com.yrkim.springwithaws.order.repository.querydsl;
+
+import com.yrkim.springwithaws.order.repository.querydsl.condition.OrderQuerydslCondition;
+
+public class OrderQuerydslRepository extends OrderQuerydslCondition {
+
+}
---
+++ src/main/java/com/yrkim/springwithaws/order/repository/querydsl/condition/OrderQuerydslCondition.java
@@ -0,0 +1,10 @@
+package com.yrkim.springwithaws.order.repository.querydsl.condition;
+
+import com.yrkim.springwithaws.common.tools.querydsl.QuerydslRepositorySupport;
+import com.yrkim.springwithaws.model.entity.OrderMaster;
+
+public class OrderQuerydslCondition extends QuerydslRepositorySupport {
+    public OrderQuerydslCondition() {
+        super(OrderMaster.class);
+    }
+}
