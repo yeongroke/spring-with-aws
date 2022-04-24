0a20559fe0f6fb3fb21ff9577cf1ff8dbf3801cd
yeongroke <chfaos120@naver.com>
2022-04-25 02:23:29 +0900

    conf: service 구조 추가

---
+++ src/main/java/com/yrkim/springwithaws/auth/service/UserService.java
@@ -0,0 +1,4 @@
+package com.yrkim.springwithaws.auth.service;
+
+public interface UserService {
+}
---
+++ src/main/java/com/yrkim/springwithaws/auth/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.yrkim.springwithaws.auth.service.impl;
+
+import com.yrkim.springwithaws.auth.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+}
