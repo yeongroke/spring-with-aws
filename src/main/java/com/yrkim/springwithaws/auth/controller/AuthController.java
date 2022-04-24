62595d64c41d6aa3f7314e545697d1ab9dc3151e
yeongroke <chfaos120@naver.com>
2022-04-25 02:22:05 +0900

    etc: auth controller 구조 추가

---
+++ src/main/java/com/yrkim/springwithaws/auth/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.yrkim.springwithaws.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * @RequiredArgsConstructor
+ * 선언된 모든 final 필드가 포함된 생성자를 생성해준다.
+ * final 없으면 생성자에 포함되지 않음.
+ *
+ * */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+}
