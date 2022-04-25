7a7c325835b85af2080b78d58f036cd6135c995f
yeongroke <chfaos120@naver.com>
2022-04-26 00:46:51 +0900

    etc: transaction config 제거

--- src/main/java/com/yrkim/springwithaws/common/config/TransactionConfig.java
+++
@@ -1,9 +0,0 @@
-package com.yrkim.springwithaws.common.config;
-
-/*@Configuration
-@ImportResource({"classpath:/aop.xml"})*/
-public class TransactionConfig {
-    public TransactionConfig() {
-
-    }
-}
