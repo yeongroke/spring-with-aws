cd8e8971aec379198570d82f3b28b7ea96a184b1
yeongroke <chfaos120@naver.com>
2022-05-16 00:18:26 +0900

    etc: custom exception 추가 및 수정

--- src/main/java/com/yrkim/springwithaws/auth/exception/UserIsExistException.java
+++ src/main/java/com/yrkim/springwithaws/auth/exception/UserIsExistException.java
@@ -3,8 +3,8 @@ package com.yrkim.springwithaws.auth.exception;
 import com.yrkim.springwithaws.common.exception.BaseRuntimeException;
 import com.yrkim.springwithaws.common.exception.response.ErrorCode;
 
-public class UserNotFoundException extends BaseRuntimeException {
-    public UserNotFoundException(ErrorCode errorCode) {
+public class UserIsExistException extends BaseRuntimeException {
+    public UserIsExistException(ErrorCode errorCode) {
         super(errorCode);
     }
 }
